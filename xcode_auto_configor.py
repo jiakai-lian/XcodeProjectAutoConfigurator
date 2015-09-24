70ef4f02ba7a9eec5069aabf546dc33c868909ee
jiakailian <jiakai.lian@gmail.com>
2015-09-24 23:50:31 +1000

    add more default settings

--- xcode_auto_configor.py
+++ xcode_auto_configor.py
@@ -3,8 +3,8 @@
 from mod_pbxproj import XcodeProject
 import sys
 
-print 'Number of arguments:', len(sys.argv), 'arguments.'
-print 'Argument List:', str(sys.argv)
+#print 'Number of arguments:', len(sys.argv), 'arguments.'
+#print 'Argument List:', str(sys.argv)
 
 if len(sys.argv) > 2:
     raise Exception("more than one file path")
@@ -29,9 +29,26 @@ for id in project["objects"][projectObject]["buildConfigurations"]:
         releaseConf = project["objects"][id]["buildSettings"]
 
 #start config
+
+#debug conf
+debugConf["WARNING_CFLAGS"] = "-Wall -Wextra -Wno-unused-parameter -Wno-unused-variable"
+debugConf["CLANG_ANALYZER_SECURITY_FLOATLOOPCOUNTER"] = "YES"
+debugConf["CLANG_ANALYZER_SECURITY_INSECUREAPI_RAND"] = "YES"
+debugConf["CLANG_ANALYZER_SECURITY_INSECUREAPI_STRCPY"] = "YES"
+debugConf["RUN_CLANG_STATIC_ANALYZER"] = "YES"
 debugConf["GCC_TREAT_WARNINGS_AS_ERRORS"] = "NO"
+
+
+#release conf
+releaseConf["WARNING_CFLAGS"] = "-Wall -Wextra -Wno-unused-parameter -Wno-unused-variable"
+releaseConf["CLANG_ANALYZER_SECURITY_FLOATLOOPCOUNTER"] = "YES"
+releaseConf["CLANG_ANALYZER_SECURITY_INSECUREAPI_RAND"] = "YES"
+releaseConf["CLANG_ANALYZER_SECURITY_INSECUREAPI_STRCPY"] = "YES"
 releaseConf["GCC_TREAT_WARNINGS_AS_ERRORS"] = "YES"
+releaseConf["RUN_CLANG_STATIC_ANALYZER"] = "YES"
+releaseConf["CLANG_STATIC_ANALYZER_MODE"] = "deep"
+
 
 project.save()
     
-print "Configuration Complete"
+print "Auto Configuration Complete"
